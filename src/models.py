c7af18da2338c093669ba3b6c46fa21c63bc9d0d
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-06-25 20:51:59 +0000

    modificaciones a los modelos

--- src/models.py
+++ src/models.py
@@ -5,51 +5,41 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
-
 Base = declarative_base()
-
-class Planet(Base):
-    __tablename__ = 'planets'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), unique=True, nullable=False)
-    climate = Column(String(50), nullable=False)
-    terrain = Column(String(50), nullable=False)
-    population = Column(Integer, nullable=False)
-    create_at = Column(DateTime(timezone=False))
-    update_at = Column(DateTime(timezone=False)) 
-    favorite = relationship('Favorite', back_populates='planets')  
-
-class Character(Base):
-    __tablename__ = 'characters'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), unique=True, nullable=False)
-    gender = Column(String(20), nullable=False)
-    birth_year = Column(String(50), nullable=False)
-    skin_color = Column(String(50), nullable=False)
-    create_at = Column(DateTime(timezone=False))
-    update_at = Column(DateTime(timezone=False))
-    favorite = relationship('Favorite', back_populates='characters')
-    
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
-    password = Column(String(50), nullable=False)
-    create_at = Column(DateTime(timezone=False))
-    update_at = Column(DateTime(timezone=False))
-    favorite = relationship('Favorite',back_populates='users', uselist=False)
-    
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    favorites = relationship("Favorite", back_populates="user")
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    climate = Column(String(50), nullable=False)
+    terrain = Column(String(50), nullable=False)
+    population = Column(Integer, nullable=False)
+
+class Character(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String(20), nullable=False)
+    birth_year = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable=False)
+
 class Favorite(Base):
     __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-    character_id = Column(Integer, ForeignKey('character.id'))
-    user = relationship('User', back_populates='favorites')
-    character = relationship('Character',back_populates='favorites', uselist=True)
-    planet = relationship('Planet',back_populates='favorites', uselist=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="favorites")
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship("Planet")
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship("Character")
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
